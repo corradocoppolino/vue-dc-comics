19a7319c4f8dae86475ea6a1256fdeb0199204e6
corradocoppo <corradocoppolino@gmail.com>
2021-05-20 20:38:31 +0200

    start

---
+++ src/components/HeaderDc.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+
+      <div class="flex-left">
+          <div class="logo">
+              <img src="../assets/dc-logo.png" alt="">
+          </div>
+      </div>
+
+      <div class="flex-right">
+        <ul>
+          <li>
+              <a href="#">characters</a>
+          </li>
+          <li>
+              <a href="#">characters</a>
+          </li>
+          <li>
+              <a href="#">characters</a>
+          </li>
+          <li>
+              <a href="#">characters</a>
+          </li>
+          <li>
+              <a href="#">characters</a>
+          </li>
+        </ul>
+      </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    .header{
+        background-color: cadetblue;
+    }
+
+    .flex-left{
+        flex: left;
+    }
+
+    .flex-right{
+        flex: right;
+    }
+
+</style>
